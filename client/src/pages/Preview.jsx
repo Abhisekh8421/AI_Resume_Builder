24348542ef4a550a5a6ce2e6a146f65b941ec86c
Suru Abhisekh <abhisekhsuru@gmail.com>
2025-10-30 16:29:27 +0530

    working on the preview link feature

--- client/src/pages/Preview.jsx
+++ client/src/pages/Preview.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { dummyResumeData } from "../assets/assets";
+import Loader from "../components/Loader";
+import ResumePreview from "../components/ResumePreview";
 
 const Preview = () => {
-  return (
-    <div>
-        <h1>Preview</h1>
+  const { resumeId } = useParams();
+  const [resumeData, setResumeData] = useState(null);
+  const loadResume = async () => {
+    setResumeData(
+      dummyResumeData.find((resume) => resume._id === resumeId || null)
+    );
+  };
+  useEffect(() => {
+    loadResume();
+  }, [resumeId]);
+  return resumeData ? (
+    <div className=" bg-slate-100">
+      <div className=" max-w-3xl mx-auto py-10">
+        <ResumePreview data={resumeData} template={resumeData.template} accentColor={resumeData.accentColor } classes=" py-4  bg-white"  />
+      </div>
     </div>
-  )
-}
+  ) : (
+    <Loader />
+  );
+};
 
-export default Preview
+export default Preview;
